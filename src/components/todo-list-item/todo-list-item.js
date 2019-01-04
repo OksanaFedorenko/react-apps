7711e8b569a85ecde94a79f9fc9c142495a784a4
Oksana Fedorenko <oksanafedorenko2016@gmail.com>
2019-01-04 23:14:10 +0200

    Added classes and states

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,31 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './todo-list-item.css'
 
-const TodoListItem = ({ label, important = false }) => {
+export default class TodoListItem extends Component {
+
+   state = {
+     done: false,
+     important: false
+   };
+   
+  /*constructor() {
+    super();
+    this.state = {
+      done: false
+    };
+  }*/
   
-const style = {
-   color: important ? 'steelblue' : 'black',
-   fontWeight: important ? 'bold' : 'normal'
+  /*Это stage-3 proposal - функция-стрелка создается на самом объекте, а не на прототипе и this сохраняет свое лексическое значение. Можно применять и другой, более консервативный, способ, закомментированный ниже.*/
+
+ onLabelClick = () => {
+   this.setState({
+     done: true
+   });
+ };
+
+ onMarkImportant = () => {
+    this.setState({
+      important: true
+    });
+ };
+
+  /*constructor() {
+      super();
+      this.onLabelClick = () => {
+        console.log(`Done: ${this.props.label}`);
+      };
+   };*/
+
+  render() {
+
+    const { label } = this.props;
+    const { done, important } = this.state;
+
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
+    }
+    if (important) {
+      classNames += ' important';
+    }
+
+
+
+    return (
+      <span className={classNames}>
+        <span 
+            className="todo-list-item-label" 
+            onClick={ this.onLabelClick }>
+          { label }
+        </span>
+        <button type="button"
+              className="btn btn-outline-success btn-sm float-right"
+              onClick = {
+                this.onMarkImportant
+              } >
+          <i className="fa fa-exclamation" />
+        </button>
+        <button type="button"
+              className = "btn btn-outline-danger btn-sm float-right">
+          <i className="fa fa-trash-o" />
+        </button>
+      </span>
+    );
+  };
 };
 
-  return (
-  <span className="todo-list-item">
-    <span 
-      className="todo-list-item-label"
-      style={style}>
-      { label }
-    </span>;
-     <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
-      </button>
+/* 
+Состояние хранится в поле state
 
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
-      </button>
-  </span>
-  );
-};
+this.state инициализируется в конструкторе  или в теле класса
 
-export default TodoListItem;
+После инициализации state нельзя изменять (только читать)
+
+Чтобы обновить state  - использовать setState()
+*/
