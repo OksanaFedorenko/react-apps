8813a8fb71621820780f53983be2120b6eb97177
Oksana Fedorenko <oksanafedorenko2016@gmail.com>
2020-04-18 21:49:25 +0200

    Added ItemList and PersonDetails

--- star-db/src/components/header/header.js
+++ star-db/src/components/header/header.js
@@ -1,25 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import './header.css';
+import "./header.css";
 
 const Header = () => {
   return (
-    <div className="header container d-flex">
-      <h3 className="app-title">
-        <a href="/">StarDB</a>
+    <div className="header d-flex">
+      <h3>
+        <a href="#/">StarDB</a>
       </h3>
-      <nav className="header-nav-menu col-6">
-        <ul className="d-flex">
-          <li><a href="">People</a></li>
-          <li><a href="">Planets</a></li>
-          <li><a href="">Starships</a></li>
-          <li><a href="">Login</a></li>
-          <li><a href="">Secret</a></li>
-        </ul>
-      </nav>
-      <button class="btn btn-primary btn-sm">Change Service></button>
+      <ul className="d-flex">
+        <li>
+          <a href="#/people">People</a>
+        </li>
+        <li>
+          <a href="#/planets">Planets</a>
+        </li>
+        <li>
+          <a href="#/starships">Starships</a>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
